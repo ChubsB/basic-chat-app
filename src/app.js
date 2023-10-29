1b0dad424c536d67c72a4af40455e8a12d38e2cb
hussain <hussainimam01@gmail.com>
2023-10-29 18:14:17 +0500

    Stage 2: Added message storage flow

--- src/app.js
+++ src/app.js
@@ -2,12 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Middlewares
+app.use(express.json());  // To parse JSON body
+
+// In-memory messages store
+const messages = [];
+
 app.get('/', (req, res) => {
     res.send('Hello, Chat App!');
+});
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
+});
+
+app.post('/messages', (req, res) => {
+    const message = req.body.message;
+    if (!message) {
+        return res.status(400).send('Message content is required');
+    }
+    messages.push(message);
+    res.status(201).send('Message added');
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-module.exports = app; // for further usage and testing
+module.exports = app;
